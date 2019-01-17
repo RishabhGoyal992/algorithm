3346008c733381269bb40c1e84a84f505c1201df
Aditya Verma <39986925+adityaverma998@users.noreply.github.com>
2019-01-17 19:55:45 +0530

    Update topological_sort.cpp

--- topological_sort.cpp
+++ topological_sort.cpp
@@ -10,86 +10,68 @@ Refer to the algo in this link - https://www.youtube.com/watch?v=ddTC4Zovtbc
 
 using namespace std;
 
-int visited[1000] = {0};
+int visited[1000];
 stack <int> s;
 
 void addedge(vector <int> v[], int s, int d){
     v[s].push_back(d);
 }
 
-void print(vector <int> v[], int n){
-    cout <<"\nADJACENCY LIST OF GRAPH\n";
-    for(int i = 0; i < n; i++){
-        cout <<i<<"-->";
-        for(int j = 0; j < v[i].size(); j++){
-            cout <<v[i][j]<<" "; 
-        }
-        cout <<"\n";
-    }
-    cout <<"\n";
-}
-
-void topo(vector <int> v[], int i){
+void toposort(vector <int> v[], int i, int n){
     if(visited[i] == 0){
-        for(int j = 0; j < v[i].size(); j++){
+        for(int j=0;j<v[i].size();j++){
             if(visited[v[i][j]] == 0){
+                toposort(v,v[i][j],n);
                 visited[v[i][j]] = 1;
-                topo(v,v[i][j]);
                 s.push(v[i][j]);
             }
         }
+    }
+}
+
+void topo(vector <int> v[], int n){
+    for(int i=0;i<n;i++){
         if(visited[i] == 0){
+            toposort(v,i,n);
             s.push(i);
             visited[i] = 1;
         }
     }
 }
 
-void topological_sort(vector <int> v[], int n){
-    for(int i = 0; i < n; i++){
-        if(visited[i] == 0){
-            topo(v,i);
-            visited[i] = 1;
-        }
+int main(){
+    cout<<"enter n : ";
+    int n;
+    cin>>n;
+    
+    cout<<"enter e :";
+    int e;
+    cin>>e;
+    
+    vector <int> v[n];
+    
+    for(int i=0;i<e;i++){
+        cout<<"enter src :";
+        int s;
+        cin>>s;
+        cout<<"enter des :";
+        int d;
+        cin>>d;
+        
+        addedge(v,s,d);
     }
-}
-
-void pf_stack(){
-    cout<<"\nprinting TOPOLOGICAL ORDERING / SORTING\n";
+    
+    for(int i=0;i<n;i++){
+        visited[i] = 0;
+    }
+    
+    cout<<"\ntopo sort :\n";
+    topo(v,n);
+    cout<<endl;
     while(!s.empty()){
         cout<<s.top()<<" ";
         s.pop();
     }
-    cout<<"\n";
-}
-
-int main()
-{
-    int n;
-    cout <<"\nenter number of vertices : \n";
-    cin>>n;
-    
-    vector <int> v[n];
-    
-    int e;
-    cout <<"\nenter number of edges : \n";
-    cin>>e;
-    
-    for(int i = 0; i < e; i++){
-        int src;
-        cout <<"\nenter source : \n";
-        cin>>src;
-        
-        int des;
-        cout <<"\nenter destination : \n";
-        cin>>des;
-        
-        addedge(v,src,des);
-    }
-
-    print(v,n);
-    topological_sort(v,n);
-    pf_stack();
-    
+    cout<<endl;
     return 0;
 }
