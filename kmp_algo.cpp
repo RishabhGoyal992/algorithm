8f674210213aaf5eba9264fd1f700f42b7c4e85e
Aditya Verma <39986925+adityaverma998@users.noreply.github.com>
2019-02-05 21:57:27 +0530

    Create kmp_algo.cpp

---
+++ kmp_algo.cpp
@@ -0,0 +1,61 @@
+//do not implement rabin karp(only read) as it is never asked in placements
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void calculatelpsarray(int lps[], char pattern[], int m){
+    int len = 0;
+    lps[0] = 0;
+    int i=1;
+    while(i<m){
+        if(pattern[i] == pattern[len]){
+            len++;
+            lps[i] = len;
+            i++;
+        }else{
+            if(len != 0){
+                len = lps[len-1];
+            }else{
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+
+void kmp(char text[], char pattern[]){
+    int n = strlen(text);
+    int m = strlen(pattern);
+    int lps[m];
+    
+    calculatelpsarray(lps,pattern,m);
+    
+    int i=0;
+    int j=0;
+    
+    while(i<n){
+        if(text[i] == pattern[j]){
+            j++;
+            i++;
+        }
+        if(j==m){
+            cout<<"pattern found at "<<i-j<<endl;
+            j = lps[j-1];
+        }else if(i<n && pattern[j] != text[i]){
+            if(j!=0){
+                j = lps[j-1];
+            }else{
+                i = i+1;
+            }
+        }
+    }
+}
+
+int main()
+{
+    char text[] = "hello whats up? how are you doing ? hello pal. i am good."; 
+    char pattern[] = "hello";
+    kmp(text,pattern);
+    return 0;
+}
